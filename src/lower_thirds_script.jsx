9aea1291a22969e6b745fb695e2390c2bd9624b1
kane <821548769@qq.com>
2018-12-23 16:42:04 +0800

    isolate createUI fuction in a separate file

--- src/lower_thirds_script.jsx
+++ src/lower_thirds_script.jsx
@@ -1,3 +1,5 @@
+#include "library/ui.jsx"
+
 function deleteAllKeyframes(property) {
     while (property.numKeys > 0) {
         property.removeKey(1);
@@ -63,23 +65,6 @@ function adjustLowerThird(comp, lowerThirdParameters) {
 };
 
 
-function createUserInterface(thisObj, userInterfaceString, scriptName) {
-    var pal = (thisObj instanceof Panel) ? thisObj : new Window("palette", scriptName,
-        undefined, { resizeable: true });
-    if (pal == null) return pal;
-
-    var UI = pal.add(userInterfaceString);
-
-    pal.layout.layout(true);
-    pal.layout.resize();
-    pal.onResizing = pal.onResize = function () {
-        this.layout.resize();
-    }
-    if ((pal != null) && (pal instanceof Window)) {
-        pal.show();
-    }
-    return UI;
-};
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////// Main
