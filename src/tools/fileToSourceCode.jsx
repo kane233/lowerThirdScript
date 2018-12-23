d08875421746c7cf4f751089d2cca095cbb88465
kane <821548769@qq.com>
2018-12-23 19:02:29 +0800

    functionalize fileToSourceCode.jsx

--- src/tools/fileToSourceCode.jsx
+++ src/tools/fileToSourceCode.jsx
@@ -1,21 +1,39 @@
 function fileToBinaryString() {
+
     var input = File.openDialog("choose a file to convert to source code");//new File("path/...")
     if (input === null) return;
 
-    input.encoding = "BINARY";
-    input.open('r');
-    var content = input.read();
-    input.close();
+    var rawData = readBianryFile(input);
+    var variableName = stringToValidVariableName(input.name);
+    var sourceCode = convertRawDataToSourceCode(rawData, variableName);
+    writeBinaryFile(input.fsName + ".jsx", sourceCode);
 
-    var contentAsString = content.toSource();
-    var variableName = input.name.replace(/\W/g, "").replace(/^\d+/, "");
-    var sourceCode = "var " + variableName + " = " + contentAsString + ";\n";
 
-    var output = new File(input.fsName + ".jsx");
-    output.open("w");
-    output.write(sourceCode);
-    output.close();
+    function convertRawDataToSourceCode(rawData, variableName) {
+        var contentAsString = rawData.toSource();
+        var sourceCode = "var " + variableName + " = " + contentAsString + ";\n";
+        return sourceCode;
+    };
 
+    function readBianryFile(file) {
+        file.encoding = "BINARY";
+        file.open('r');
+        var content = file.read();
+        file.close();
+        return content;
+    };
+
+    function writeBinaryFile(filePath, content) {
+        var output = new File(filePath);
+        output.open("w");
+        output.write(sourceCode);
+        output.close();
+    };
+
+    function stringToValidVariableName(string) {
+        return string.replace(/\W/g, "")  //remove anything that is not letter,digit or _
+        .replace(/^\d+/, "");  //remove any digits at the beginning
+    };
 }
 
 fileToBinaryString();
