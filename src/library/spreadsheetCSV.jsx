f910acbd084e25be0077934e996bb25e9fbe3ad9
kane <45994097+kane233@users.noreply.github.com>
2018-12-26 17:55:00 +0800

    do some change

--- src/library/spreadsheetCSV.jsx
+++ src/library/spreadsheetCSV.jsx
@@ -1,5 +1,80 @@
 function loadCsvFormFile(file, options) {
-    option = option || {};
+    function parseCSVString(csvString, options) {
+        options = options || {};
+        var separatorSymbol = options.separatorSymbol || ",";
+
+        var currentRow = 0;
+        var currentColumn = -1; //will be increased to 0 during first loop iteration
+        var thisCell = "";
+        var i;
+        var token;
+        var cellStart = true;
+        var insideQuote;
+
+
+        function nextTokenLookAhead() {
+            var nexTokenId = i + 1;
+            return (nexTokenId < csvString.length ? csvString[nexTokenId] : "")
+        };
+
+        function processLineBreak() {
+            addCSVCellToData(thisCell, currentRow, currentColumn);
+            cellStart = true;
+            currentRow++;
+            currentColumn = -1;
+        };
+
+        function processCellStart() {
+            thisCell = "";
+            currentColumn++;
+            insideQuote = (token == '"');
+            if (insideQuote) {
+                i++
+                token = csvString[i]
+            }
+            cellStart = false;
+        };
+
+        for (i = 0; i < csvString.length; i++) {
+            token = csvString[i];
+            if (cellStart) {
+                processCellStart();
+            }
+
+            if (!insideQuote && token == separatorSymbol) {
+                addCSVCellToData(thisCell, currentRow, currentColumn);
+                cellStart = true;
+            } else if (!insideQuote && token == "\n") {
+                processLineBreak();
+            } else if (!insideQuote && token == "\r" && nextTokenLookAhead() == "\n") {
+                i++;
+                processLineBreak();
+            } else if (insideQuote && token == '"') {
+                if (nextTokenLookAhead() == '"') {
+                    i++;
+                    thisCell += '"';
+                } else {
+                    insideQuote = false;
+                }
+            } else {
+                thisCell += token;
+            }
+        }
+    };
+
+    function addCSVCellToData(cellData, row, column) {
+        if (data == undefined) {
+            data = [];
+        }
+        if (data[row] == undefined) {
+            data[row] = [];
+        }
+
+        data[row][column] = cellData;
+    };
+
+    var data;
+    options = options || {};
     file.encoding = options.encoding || "utf-8";
     if (!file.open("r")) {
         throw new Error("could not open file:" + file.error);
@@ -8,39 +83,8 @@ function loadCsvFormFile(file, options) {
     file.close();
     if (file.error != "") throw new Error("could not read file:" + file.error);
 
-    return parseCSVString(rawData, options);
-};
-
-function parseCSVString(csvString, options) {
-    option = option || {};
-    var separatorSymbol = options.separatorSymbol || ",";
-
-    var currentRow = 0;
-    var currentColumn = -1; //will be increased to 0 during first loop iteration
-    var thisCell = "";
-    var i;
-    var token;
-    var cellStart = true;
-    var insideQuote;
-
-    function processCellStart() {
-        thisCell = "";
-        currentColumn++;
-        insideQuote = (token == '"');
-        if (insideQuote) {
-            i++
-            token = csvString[i]
-        }
-    };
-
-    for (i = 0; i < csvString.length; i++) {
-        token = csvString[i];
-        if (cellStart) {
-            processCellStart();
-        }
-    }
-
+    parseCSVString(rawData, options);
+    return data;
 };
 
 
-loadCsvFormFile(file)
